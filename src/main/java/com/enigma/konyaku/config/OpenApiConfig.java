4c004669b65cae6bb2a912bf0cb4ce93c678e840
Yeremi Bento <yeremibsb@gmail.com>
2024-05-10 16:58:15 +0700

    fix swagger

---
+++ src/main/java/com/enigma/konyaku/config/OpenApiConfig.java
@@ -0,0 +1,42 @@
+package com.enigma.konyaku.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Saiful Rahman",
+                        email = "sr.perante@gmail.com"
+                ),
+                description = "Spring Security WMB API documentation",
+                title = "WMB API - Rahman",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local WMB",
+                        url = "http://localhost:8080"
+                )
+        },
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+
+}
